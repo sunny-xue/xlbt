de9999e676644ab20fc7ab157403a7f10b2c0c7b
xsysunny <xsysunny@163.com>
2016-11-22 11:30:30 +0800

    翔龙鞭陀网源码

---
+++ data/common.inc.php
@@ -0,0 +1,11 @@
+<?php
+//数据库连接信息
+$cfg_dbhost = 'q7ovwmbz.zzcdb.dnstoo.com:5509';
+$cfg_dbname = 'xlbt';
+$cfg_dbuser = 'xlbt_f';
+$cfg_dbpwd = 'xlbt123';
+$cfg_dbprefix = 'dede_';
+$cfg_db_language = 'gbk';
+
+
+?>
---
+++ skin/js/a.js
@@ -0,0 +1,57 @@
+﻿$(function(){
+
+    $(function () {
+        $('.wx').click(function () {
+            var isOpen = 0;
+            $('.box-mask').fadeIn(500);
+            center($('.box'));
+            $('.btnCancel').click(function () {
+                $('.box-mask').fadeOut(500);
+                $('.box').fadeOut(500);
+                isOpen = 0;
+            });
+        });
+
+    })
+    function center(obj) {
+        var screenWidth = $(window).width(), screenHeigth = $(window).height();
+        var scollTop = $(document).scrollTop();
+        var objLeft = (screenWidth - obj.width()) / 2;
+        var objTop = (screenHeigth - obj.height()) / 2 + scollTop;
+        obj.css({
+            left: objLeft + "px",
+            top: objTop + "px"
+        });
+        obj.fadeIn(500);
+        isOpen = 1;
+        $(window).resize(function () {
+            if (isOpen == 1) {
+                screenWidth = $(window).width();
+                screenHeigth = $(window).height();
+                var scollTop = $(document).scrollTop();
+                objLeft = (screenWidth - obj.width()) / 2;
+                var objTop = (screenHeigth - obj.height()) / 2 + scollTop;
+                obj.css({
+                    left: objLeft + "px",
+                    top: objTop + "px"
+                });
+                obj.fadeIn(500);
+            }
+        });
+        $(window).scroll(function () {
+            if (isOpen == 1) {
+                screenWidth = $(window).width();
+                screenHeigth = $(window).height();
+                var scollTop = $(document).scrollTop();
+                objLeft = (screenWidth - obj.width()) / 2;
+                var objTop = (screenHeigth - obj.height()) / 2 + scollTop;
+                obj.css({
+                    left: objLeft + "px",
+                    top: objTop + "px"
+                });
+                //obj.fadeIn(500);
+            }
+        });
+    }
+
+});
